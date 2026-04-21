6ce2284bed4a330242793743d56664b0a1a36118
jjkamantauskas <160356196+jjkamantauskas@users.noreply.github.com>
2026-04-21 03:14:53 -0500

    test passed
    updated routes to note if something requires authentication to access

--- routes/photoRoutes.js
+++ routes/photoRoutes.js
@@ -15,7 +15,7 @@ function isValidObjectId(id) {
  * GET /photosOfUser/:id
  * Returns all photos of the given user.
  */
-router.get('/photosOfUser/:id', async (req, res) => {
+router.get('/photosOfUser/:id', requireAuth, async (req, res) => {
   const id = req.params.id;
 
   if (!isValidObjectId(id)) {
